4714008d04ed7362053da01e199651d93c30cd57
benekeneb <54267526+benekeneb@users.noreply.github.com>
2020-12-07 15:06:21 +0100

    ADD: Projektfiles

---
+++ infoscreen.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>WHG INFOMONITOR</title>
+
+      <link href="infoscreen.css" type="text/css" rel="stylesheet">
+  </head>
+  <body>
+    <!-- <div class="header">
+      WGH INFOMONITOR
+    </div> -->
+    <div class="content_middle">
+      <div class="left">
+        <!-- <iframe src="inc/weihnachtsfeier.html" width="" height=""></iframe> -->
+        <!-- <object data="inc/weihnachtsfeier.svg" type="image/svg+xml"> -->
+          <img src="inc/weihnachtsfeier.svg" alt="Loading">
+
+      </div>
+      <div class="right">
+        <div class="mvv-departure-monitor" monitor-configuration="eyJsYW5ndWFnZSI6eyJkZXBhcnR1cmUiOiJBYmZhaHJ0IiwidHJhaW5TdG9wcyI6IkhhbHRlc3RlbGxlbiIsImRpcmVjdGlvbiI6IlJpY2h0dW5nIiwiZm9vdGVyTm90ZSI6IqkgQ29weXJpZ2h0IiwiZm9vdGVyVGV4dCI6IldlaXRlcmUgRmFocnBsYW5hdXNr/G5mdGUgdW50ZXIgd3d3Lm12di1hdXNrdW5mdC5kZSBvZGVyIG1pdCBkZXIgTVZWLUFwcCIsImhlYWRlclRleHQiOiJBYmZhaHJ0ZW4gZvxyIGhldXRlLCAiLCJsYW5ndWFnZSI6ImRlIiwibGluZSI6IkxpbmllIiwibGl2ZSI6IkxpdmUiLCJzdG9wIjoiSGFsdGVzdGVsbGUiLCJ0cmFjayI6IkdsZWlzIn0sImlzRnVsbHNjcmVlbiI6ZmFsc2UsInN0YXRpb25zIjpbeyJzdGF0aW9uIjp7Im5hbWUiOiJN/G5jaGVuLCBIb2hlbnpvbGxlcm5wbGF0eiIsImlkIjoiZGU6MDkxNjI6MTQwIiwiYW55VHlwZSI6InN0b3AiLCJzdGF0ZWxlc3MiOiI5MTAwMDE0MCIsInJlZiI6eyJpZCI6IjkxMDAwMTQwIiwiZ2lkIjoiZGU6MDkxNjI6MTQwIiwib21jIjoiOTE2MjAwMCIsInBsYWNlSUQiOiIxIiwicGxhY2UiOiJN/G5jaGVuIiwiY29vcmRzIjoiMTI4NzgyOS4wMDAwMCw1ODY2MDkzLjAwMDAwIn19LCJsaW5lcyI6W3sibnVtYmVyIjoiVTIiLCJzeW1ib2wiOiIwMTBVMi5zdmciLCJkaXJlY3Rpb24iOiJNZXNzZXN0YWR0IE9zdCIsInN0YXRlbGVzcyI6InN3bTowMTBVMjpHOkg6MDEwIiwibmFtZSI6IlUtQmFobiJ9LHsibnVtYmVyIjoiVTIiLCJzeW1ib2wiOiIwMTBVMi5zdmciLCJkaXJlY3Rpb24iOiJGZWxkbW9jaGluZyIsInN0YXRlbGVzcyI6InN3bTowMTBVMjpHOlI6MDEwIiwibmFtZSI6IlUtQmFobiJ9LHsibnVtYmVyIjoiMTIiLCJzeW1ib2wiOiIwMjAxMi5zdmciLCJkaXJlY3Rpb24iOiJSb21hbnBsYXR6Iiwic3RhdGVsZXNzIjoic3dtOjAyMDEyOkc6SDowMTAiLCJuYW1lIjoiVHJhbSJ9LHsibnVtYmVyIjoiMTIiLCJzeW1ib2wiOiIwMjAxMi5zdmciLCJkaXJlY3Rpb24iOiJTY2hlaWRwbGF0eiBVIiwic3RhdGVsZXNzIjoic3dtOjAyMDEyOkc6UjowMTAiLCJuYW1lIjoiVHJhbSJ9LHsibnVtYmVyIjoiMjciLCJzeW1ib2wiOiIwMjAyNy5zdmciLCJkaXJlY3Rpb24iOiJTZW5kbGluZ2VyIFRvciBVIHZpYSBLYXJsc3BsYXR6IFUgUyIsInN0YXRlbGVzcyI6InN3bTowMjAyNzpHOkg6MDEwIiwibmFtZSI6IlRyYW0ifSx7Im51bWJlciI6IjI3Iiwic3ltYm9sIjoiMDIwMjcuc3ZnIiwiZGlyZWN0aW9uIjoiUGV0dWVscmluZyBVIiwic3RhdGVsZXNzIjoic3dtOjAyMDI3Okc6UjowMTAiLCJuYW1lIjoiVHJhbSJ9LHsibnVtYmVyIjoiTjI3Iiwic3ltYm9sIjoiMjJOMjcuc3ZnIiwiZGlyZWN0aW9uIjoiR3Jv32hlc3NlbG9oZXIgQnL8Y2tlIiwic3RhdGVsZXNzIjoic3dtOjAyTjI3Okc6SDowMTAiLCJuYW1lIjoiVHJhbSJ9LHsibnVtYmVyIjoiTjI3Iiwic3ltYm9sIjoiMjJOMjcuc3ZnIiwiZGlyZWN0aW9uIjoiUGV0dWVscmluZyIsInN0YXRlbGVzcyI6InN3bTowMk4yNzpHOlI6MDEwIiwibmFtZSI6IlRyYW0ifSx7Im51bWJlciI6IjUzIiwic3ltYm9sIjoiMDMwNTMuc3ZnIiwiZGlyZWN0aW9uIjoiQWlkZW5iYWNoc3RyYd9lIFUgdmlhIFJvdGtyZXV6cGxhdHogVSIsInN0YXRlbGVzcyI6InN3bTowMzA1MzpHOkg6MDEwIiwibmFtZSI6IkJ1cyJ9LHsibnVtYmVyIjoiNTMiLCJzeW1ib2wiOiIwMzA1My5zdmciLCJkaXJlY3Rpb24iOiJN/G5jaG5lciBGcmVpaGVpdCBVIiwic3RhdGVsZXNzIjoic3dtOjAzMDUzOkc6UjowMTAiLCJuYW1lIjoiQnVzIn0seyJudW1iZXIiOiI1OSIsInN5bWJvbCI6IjAzMDU5LnN2ZyIsImRpcmVjdGlvbiI6IkdpZXNpbmcgQmYgVSBTIHZpYSBN/G5jaG5lciBGcmVpaGVpdCBVIiwic3RhdGVsZXNzIjoic3dtOjAzMDU5Okc6SDowMTAiLCJuYW1lIjoiQnVzIn0seyJudW1iZXIiOiI1OSIsInN5bWJvbCI6IjAzMDU5LnN2ZyIsImRpcmVjdGlvbiI6IkFja2VybWFubmJvZ2VuIiwic3RhdGVsZXNzIjoic3dtOjAzMDU5Okc6UjowMTAiLCJuYW1lIjoiQnVzIn0seyJudW1iZXIiOiJONDMiLCJzeW1ib2wiOiIwM040My5zdmciLCJkaXJlY3Rpb24iOiJPc3RiYWhuaG9mIiwic3RhdGVsZXNzIjoic3dtOjAzTjQzOkc6SDowMTAiLCJuYW1lIjoiQnVzIn0seyJudW1iZXIiOiJONDQiLCJzeW1ib2wiOiIwM040NC5zdmciLCJkaXJlY3Rpb24iOiJPc3RiYWhuaG9mIiwic3RhdGVsZXNzIjoic3dtOjAzTjQ0Okc6SDowMTAiLCJuYW1lIjoiQnVzIn1dLCJsZWFkVGltZU1pbnV0ZXMiOjB9XSwibGluZXMiOltdLCJtYXhSZXN1bHRzIjoxMCwiZmV0Y2hJbnRlcnZhbEluTWludXRlcyI6Miwic2hvd05vdGlmaWNhdGlvbiI6dHJ1ZX0="></div>
+        <script type="text/javascript" src="https://www.mvv-muenchen.de/typo3conf/ext/sn_mvv_efa/Resources/Public/mvv-monitor/mvv-monitor.min.js"></script>
+
+
+        <div class="mvv-monitor-container">
+          <div class="mvv-monitor-header-container">
+            <div class="mvv-monitor-header">
+              <h3>Abfahrten für heute,  00 02 Uhr <i class="mvv-monitor-spinner"></i></h3>
+              <img src="https://www.mvv-muenchen.de/typo3conf/ext/sn_config_mu17/Resources/Public/Images/mvv-logo.png" style="cursor: pointer;" alt="MVV Logo">
+            </div>
+            <div class="mvv-monitor-subheader" style="padding: 0 1em;">
+              <p>Termine</p>
+            </div>
+          </div>
+
+          <div class="mvv-monitor-departures" style="font-size: 105%;">
+            <table>
+              <tr>
+                <td><b>Studentenrat</b></td>
+                <td style="text-align: right;">10.12.2020 19:30</td>
+              </tr>
+              <tr>
+                <td><b>Heimrat</b></td>
+                <td style="text-align: right;">16.12.2020 19:30</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="content_bottom">
+
+    </div>
+  </body>
+</html>
