374ada39b9490b0e27bd28862e9e1ac5a059e751
powman1 <50322338+powman1@users.noreply.github.com>
2021-01-31 17:48:33 +0100

    added Senior Anoucuments

--- infoscreen.js
+++ infoscreen.js
@@ -36,7 +36,7 @@ showTime();
 
 //NOTE: Diashow
 //anouncement Ordner kann nicht von Javascript ausgelesen werden, deswegen sind alle Dateien im folgenden Array gespeichert
-var file_array = ['Secret Hitler Plakat.svg', 'Tutorenfahrt Save the Date.svg', 'Vollversammlung Plakat.svg']
+var file_array = ['Secret Hitler Plakat.svg', 'Einladung Heimrat.jpeg', 'Tutorenfahrt Save the Date.svg', 'Einladung Vollversammlung.jpeg']
 
 var counter = 0;
 function changeAnouncement(index){
